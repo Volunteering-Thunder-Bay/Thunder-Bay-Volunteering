27d4a10c4deabff92da2a13e83e8d62aedeeacc9
Volunteering-Thunder-Bay <112196807+Volunteering-Thunder-Bay@users.noreply.github.com>
2022-09-03 18:58:00 -0400

    Update engine.py

--- engine.py
+++ engine.py
@@ -3,7 +3,6 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import pickle
 import pandas as pd
-from IPython.display import HTML
 indices = pickle.load(open('indices', 'rb'))
 sim_matrix = pickle.load(open('sim_matrix', 'rb'))
 from PIL import Image
